3feeeaceae75107a0eaf40dd3a608ee4a69280dc
chalolowa <wangwelocha@students.uonbi.ac.ke>
2024-02-25 19:41:14 +0300

    On branch main
     Initial commit
    
     Changes to be committed:
    	new file:   user_input.py

---
+++ user_input.py
@@ -0,0 +1,5 @@
+name = input("What is your name?")
+age = input("What is your age?")
+location = input("Whwere do you live?")
+
+print('Hello ' + name + ', you are ' + age + ' years old and live in ' + location + '.')
